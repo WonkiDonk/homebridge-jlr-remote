af84519840b49f4082ae7c6433dc878962063231
William Cowell <william.cowell@musclefood.com>
2021-02-19 11:16:58 +0000

    JLR Remote Platform Plugin Logs that the component finished launching

--- src/JaguarLandRoverRemotePlatformPlugin.ts
+++ src/JaguarLandRoverRemotePlatformPlugin.ts
@@ -1,8 +1,12 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformConfig, PlatformAccessory } from 'homebridge'
+import { API, DynamicPlatformPlugin, Logger, PlatformConfig, PlatformAccessory, APIEvent } from 'homebridge'
 
 export default class JaguarLandRoverRemotePlatformPlugin implements DynamicPlatformPlugin {
     constructor(log: Logger, config: PlatformConfig, api: API) {
         log.debug('Jaguar Land Rover Remote Platform Plugin loaded.')
+
+        api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+            log.debug('Jaguar Land Rover Platform `didFinishLaunching`')
+        })
     }
 
     configureAccessory = (accessory: PlatformAccessory): void => {
--- tests/unit/JaguarLandRoverRemotePlatformPlugin.test.ts
+++ tests/unit/JaguarLandRoverRemotePlatformPlugin.test.ts
@@ -2,7 +2,7 @@ import {createMock} from 'ts-auto-mock'
 import JaguarLandRoverRemotePlatformPlugin from '../../src/JaguarLandRoverRemotePlatformPlugin'
 import {Logger, PlatformConfig, API} from 'homebridge'
 
-test('logs out that the platform has been instantiated', () => {
+test('logs that the platform has been instantiated', () => {
     const mockLog = createMock<Logger>()
 
     new JaguarLandRoverRemotePlatformPlugin(
@@ -12,4 +12,20 @@ test('logs out that the platform has been instantiated', () => {
 
     expect(mockLog.debug)
         .toHaveBeenCalledWith('Jaguar Land Rover Remote Platform Plugin loaded.')
+})
+
+test('logs that the component finished launching', () => {
+    const on = jest.fn()
+    on.mockImplementation((event: string, listener: () => void) => {if(event === 'didFinishLaunching') {listener()}})
+
+    const mockLog = createMock<Logger>()
+    const mockApi = createMock<API>({on})
+
+    new JaguarLandRoverRemotePlatformPlugin(
+        mockLog,
+        createMock<PlatformConfig>(),
+        mockApi)
+
+    expect(mockLog.debug)
+        .toHaveBeenCalledWith('Jaguar Land Rover Platform `didFinishLaunching`')
 })
